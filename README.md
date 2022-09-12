6248b06236cc3fbbfa8b86f3c14d426691f920af
Lunu Bounir <lunu.bounir@gmail.com>
2022-09-12 01:12:13 -0400

    version 0.1.0

---
+++ README.md
@@ -0,0 +1,13 @@
+## "notification-view" custom element
+
+This is a "notification-view" custom element
+
+## Usage Instruction
+
+https://webextension.org/custom-component/notification-view/index.html
+
+![img](https://github.com/lunu-bounir/notification-view/blob/main/screenshot.png?raw=true)
+
+## License
+
+"notification-view" custom element is an open-source project released under [MPL-2.0](https://github.com/lunu-bounir/notification-view/blob/master/LICENSE)
---
+++ example/index.js
@@ -0,0 +1,6 @@
+const e = document.querySelector('notification-view');
+
+document.getElementById('info').onclick = () => e.notify('this is my message' + Math.random(), 'info');
+document.getElementById('warning').onclick = () => e.notify('this is my message' + Math.random(), 'warning');
+document.getElementById('error').onclick = () => e.notify('this is my message' + Math.random(), 'error');
+document.getElementById('success').onclick = () => e.notify('this is my message' + Math.random(), 'success');
